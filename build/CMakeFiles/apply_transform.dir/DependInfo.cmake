7e972c595ab848bd640013ef39ab44fa58a6e35d
evan.o-keeffe <evan.o-keeffe@ucdconnect.ie>
2019-05-22 09:06:03 +0100

    backing up code for geodectic transform

---
+++ build/CMakeFiles/apply_transform.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evan/Contracting/ProvEye/WorkingDirectory/PLY_Transform/src/transform.cpp" "/home/evan/Contracting/ProvEye/WorkingDirectory/PLY_Transform/build/CMakeFiles/apply_transform.dir/src/transform.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/pcl-1.9"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/apply_transform.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apply_transform.dir/src/transform.cpp.o"
+  "apply_transform.pdb"
+  "apply_transform"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apply_transform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
